050991e1ff59448391e9cce16c7185dafea1dca4
itsammarmirza <itsammarmirza@gmail.com>
2023-06-26 15:39:01 +0530

    Created a Netflix Homepage for mobile-screens

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'src/microsoft-clone/index.html'),
+        nested: resolve(__dirname, 'index.html'),
       },
     },
   },
